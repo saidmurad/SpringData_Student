98f350c7c10ee051dbe750792d3444a0251953b7
saidmurad <saidmurad50@gmail.com>
2023-09-28 14:18:53 +0300

    change the entity manager implementation to JPA repository implementation

--- src/main/java/com/student/service/StudentServiceImpl.java
+++ src/main/java/com/student/service/StudentServiceImpl.java
@@ -3,6 +3,7 @@ package com.student.service;
 import java.util.Collection;
 import java.util.Queue;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -10,29 +11,27 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import com.student.core.Student;
+import com.student.repository.StudentRepository;
 
 @Named
 public class StudentServiceImpl implements StudentService {
 
-	@PersistenceContext
-	EntityManager em;
+	@Inject
+	StudentRepository studentRepository;
 
 	@Override
 	public Student get(long id) {
-		return em.find(Student.class, id);
+		return studentRepository.findById(id).get();
 	}
 
 	@Override
 	public Collection<Student> getAllStudents() {
-		TypedQuery<Student> q = em.createQuery("FROM Student", Student.class);
-		return q.getResultList();
+		return studentRepository.findAll();
 	}
 
 	@Override
 	public Collection<Student> getStudentsByDepartment(String departemnt) {
-		TypedQuery<Student> q = em.createQuery("FROM Student student WHERE student.dept = :dept", Student.class);
-		q.setParameter("dept", departemnt);
-		return q.getResultList();
+		return studentRepository.findByDept(departemnt);
 	}
 
 
